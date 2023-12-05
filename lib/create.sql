4345147f15fc1fdd1466bea662f295585978b887
Elijah Ngocho <elijahngocho24@gmail.com>
2023-12-06 00:13:01 +0300

    passed

---
+++ lib/create.sql
@@ -0,0 +1,4 @@
+CREATE TABLE bears (
+id INTEGER, name TEXT, age INTERGER, sex TEXT, color TEXT, 
+temperament TEXT, alive BOOLEAN
+);
---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES 
+(1, "Mr. Chocolate", 3, "M", "Low", True),
+(2, "Rowdy", 2, "M", "High", False),
+(3, "Sergeant Brown", 4, "M", "Low", True),
+(4, "Melissa", 9, "F", "High", True),
+(5, "Grinch", 3, "M", "Low", False),
+(6, "Wendy", 4, "F", "Low", True),
+(7, NULL, 3, "M", "High", False);
